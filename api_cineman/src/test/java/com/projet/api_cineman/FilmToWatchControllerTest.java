9d567df06bf75b243794863957c714c5c3bce96e
geoffreyrig4l <grigal@oui.sncf>
2021-11-09 10:03:11 +0100

    test filmToWatch

---
+++ api_cineman/src/test/java/com/projet/api_cineman/FilmToWatchControllerTest.java
@@ -0,0 +1,99 @@
+package com.projet.api_cineman;
+
+import com.projet.api_cineman.controller.FilmToWatchController;
+import com.projet.api_cineman.model.FilmToWatch;
+import com.projet.api_cineman.repository.FilmToWatchRepository;
+import com.projet.api_cineman.service.FilmToWatchService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FilmToWatchControllerTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @TestConfiguration
+    static class FilmToWatchControllerTestConfig{
+
+        @Bean
+        @Primary        //creer un autre repo qui est prioritaire sur le VRAI repo, les tests seront exeutés sur ce dernier
+                    //l'objectif etant de teste sans modifier notre bdd
+        public FilmToWatchRepository filmToWatchRepositoryTest(){
+            FilmToWatchRepository filmToWatchRepository = mock(FilmToWatchRepository.class); //simuler le comportement d'un objet , ici le film to watch , il ne sera pas instancie mais mocké
+            //film 1
+            FilmToWatch filmToWatch = new FilmToWatch();
+            filmToWatch.setId(1L);
+            filmToWatch.setTitle("Harry Potter");
+            filmToWatch.setDate_released("2001");
+            when(filmToWatchRepository.findById(1L)).thenReturn(Optional.of(filmToWatch));
+            //film 2
+            FilmToWatch filmToWatch2 = new FilmToWatch();
+            filmToWatch2.setId(2L);
+            filmToWatch2.setTitle("Harry Potter 2");
+            filmToWatch2.setDate_released("2002");
+            when(filmToWatchRepository.findById(2L)).thenReturn(Optional.of(filmToWatch2));
+            //liste films
+            List<FilmToWatch> listFilms = List.of(filmToWatch, filmToWatch2);
+            when(filmToWatchRepository.findAll()).thenReturn(listFilms);
+            return filmToWatchRepository;
+        }
+    }
+
+    @Test
+    void test_get_all_films_to_watch() throws Exception{
+        mockMvc.perform(get("/films-to-watch"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title",is("Harry Potter")));
+    }
+
+    @Test
+    void should_get_one_film_to_watch() throws Exception{
+        mockMvc.perform(get("/films-to-watch/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title",is("Harry Potter")));
+    }
+
+    @Test
+    void should_not_get_one_film_to_watch() throws Exception{
+        mockMvc.perform(get("/films-to-watch/2"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void should_put_one_film_to_watch() throws Exception{
+        mockMvc.perform(put("/films-to-watch/1")
+                        .content("{\"id\":1,\"title\":\"Harry Potter\",\"date_released\":\"3000\"}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void should_not_put_one_film_to_watch() throws Exception{
+        mockMvc.perform(put("/films-to-watch/2")
+                        .content("{\"id\":1,\"title\":\"Harry Potter\",\"date_released\":\"3000\"}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());        //badRequest comme dans la methode update de filmtowatchcontroller
+    }
+}
